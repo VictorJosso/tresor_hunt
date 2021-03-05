2e121ce0858734f319ecaf3f39549a3ca5fd6687
JOSSO victor <josso@informatique.univ-paris-diderot.fr>
2021-03-05 14:46:45 +0100

    Ajout du fichier pour build le serveur (Linux)

---
+++ build_serveur.sh
@@ -0,0 +1,11 @@
+#usr/bin/bash
+
+set -o allexport
+[[ -f .env ]] && source .env
+set +o allexport
+
+if [ -Z $JAVA_PATH ] || [ -Z $JAVAC_PATH ]
+then echo "Merci de completer le fichier .env avant d'executer ce script".
+exit 1
+fi
+javac -d out/production/Serveur $(find Serveur/src/ -name "*.java")
